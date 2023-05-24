b69dd67228eb64a1199a7ccde64aea98f1a3b617
Paul Ross <pross326@gmail.com>
2023-05-24 15:24:14 -0400

    flexbox working better, alt text broken

--- myscript.js
+++ myscript.js
@@ -27,46 +27,57 @@ function applyAltTextChanges() {
     }
 }
 
-// Function for the "table to flexbox" page
 function convertTableToFlexbox() {
     var htmlInput = document.getElementById('table-html-input').value;
-    var paddingSliderValue = document.getElementById('padding-slider').value;
-    var marginSliderValue = document.getElementById('margin-slider').value;
-
+    var paddingSlider = document.getElementById('padding-slider');
+    var marginSlider = document.getElementById('margin-slider');
+    var paddingValue = document.getElementById('padding-value');
+    var marginValue = document.getElementById('margin-value');
+  
     var parser = new DOMParser();
     var doc = parser.parseFromString(htmlInput, 'text/html');
     var table = doc.querySelector('table');
-
+  
     if (table) {
         var flexContainer = doc.createElement('div');
         flexContainer.style.display = 'flex';
         flexContainer.style.flexWrap = 'wrap';
-
+  
         var tableRows = Array.from(table.rows);
+        var numColumns = tableRows[0].cells.length;
+  
         tableRows.forEach(function(row) {
             var flexRow = doc.createElement('div');
             flexRow.style.display = 'flex';
             flexRow.style.width = '100%';
-
+  
             var tableCells = Array.from(row.cells);
+            var cellWidth = 100 / numColumns + '%';
+  
             tableCells.forEach(function(cell) {
                 var flexCell = doc.createElement('div');
-                flexCell.style.flex = '1 0 auto';
-                flexCell.style.padding = paddingSliderValue + 'px';
-                flexCell.style.margin = marginSliderValue + 'px';
+                flexCell.style.width = cellWidth;
                 flexCell.style.textAlign = 'center';
-                flexCell.innerHTML = cell.innerHTML;
+                flexCell.style.border = '1px solid black'; // Add border style
+
+                var pTag = doc.createElement('p');
+                pTag.style.padding = paddingSlider.value + 'px';
+                pTag.style.margin = marginSlider.value + 'px';
+                pTag.style.boxSizing = 'border-box';
+                pTag.innerHTML = cell.innerHTML;
+
+                flexCell.appendChild(pTag);
                 flexRow.appendChild(flexCell);
             });
-
+  
             flexContainer.appendChild(flexRow);
         });
-
+  
         table.parentNode.replaceChild(flexContainer, table);
-
+  
         var updatedHtml = doc.body.innerHTML;
-
-        document.getElementById('table-html-output').innerText = htmlInput
+  
+        document.getElementById('table-html-output').innerText = htmlInput;
         document.getElementById('table-render-output').innerHTML = htmlInput;
         document.getElementById('table-updated-html').innerText = updatedHtml;
         document.getElementById('table-rendered-updated-html').innerHTML = updatedHtml;
@@ -74,6 +85,44 @@ function convertTableToFlexbox() {
         alert('Invalid input. Please enter a valid table.');
     }
 }
+
+
+  
+  
+
+
+  
+  function resetTableFlexbox() {
+    document.getElementById('table-html-input').value = `<table>
+    <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+    </tr>
+    <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+    </tr>
+</table>`;
+    document.getElementById('table-html-output').innerText = '';
+    document.getElementById('table-render-output').innerHTML = '';
+    document.getElementById('table-updated-html').innerText = '';
+    document.getElementById('table-rendered-updated-html').innerHTML = '';
+  }
+  
+  // Attach the functions to the respective buttons on the page
+  window.onload = function() {
+    document.getElementById('apply-changes-table-flexbox').onclick = convertTableToFlexbox;
+    document.getElementById('reset-table-flexbox').onclick = resetTableFlexbox;
+  };
+  
+  // Update slider values on input change
+  document.getElementById('padding-slider').addEventListener('input', function() {
+    document.getElementById('padding-value').textContent = this.value + 'px';
+  });
+  
+  document.getElementById('margin-slider').addEventListener('input', function() {
+    document.getElementById('margin-value').textContent = this.value + 'px';
+  });
 
 
 function resetAltTextPage() {
@@ -93,14 +142,7 @@ function resetAltTextPage() {
     // Scroll to the top of the page
     window.scrollTo(0, 0);
 }
-// Function to reset the page
-function resetTableFlexbox() {
-    document.getElementById('table-html-input').value = '';
-    document.getElementById('table-html-output').innerText = '';
-    document.getElementById('table-render-output').innerHTML = '';
-    document.getElementById('table-updated-html').innerText = '';
-    document.getElementById('table-rendered-updated-html').innerHTML = '';
-}
+
 
 // Attach the functions to the respective buttons on the page
 window.onload = function() {
